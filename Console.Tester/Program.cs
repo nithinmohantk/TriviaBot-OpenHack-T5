bdf5a4633b9156a48c2b08a457f877a6fcca5690
nithin.mohan <nithin.mohan@pramerica.ie>
2019-01-23 10:04:47 +0000

    added new console app.

---
+++ Console.Tester/Program.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            APITester.SubmitAnswer("e268e894-b242-4c0c-876c-8edba523ca3a", "1035", "4138");
+
+            Console.Read();
+        }
+
+    }
+
+
+    public static class APITester
+        {
+
+           static HttpClient httpClient = new HttpClient();
+
+            static APITester()
+            {
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "application/json");
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json; charset=utf-8");
+                httpClient.DefaultRequestHeaders
+                  .Accept
+                  .Add(new MediaTypeWithQualityHeaderValue("application/json"));//ACCEPT header
+        }
+
+            public static async Task  SubmitAnswer(string Id, string questionId, string answerId)
+            {
+                var sb = new StringBuilder();
+                sb.Append("{");
+                sb.Append("  \"userId\": \"\"" + Id + "\"");
+                sb.Append("  \"questionId\": \"\"" + questionId + "\"");
+                sb.Append("  \"answerId\": \"\"" + answerId + "\"");
+                sb.Append("}");
+
+                var response = await httpClient.PostAsync("https://msopenhack.azurewebsites.net/api/trivia/answer",
+                    new StringContent(sb.ToString()));
+
+                response.EnsureSuccessStatusCode();
+
+                string respJson = await response.Content.ReadAsStringAsync();
+
+                JObject jObj = JsonConvert.DeserializeObject<JObject>(respJson);
+            }
+    }
+}
