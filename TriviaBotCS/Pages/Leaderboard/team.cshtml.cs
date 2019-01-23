190b30fc3839078322dc11af73f4ddfbfa42c788
nithin.mohan <nithin.mohan@pramerica.ie>
2019-01-23 15:58:58 +0000

    initial commit tab changes

---
+++ TriviaBotCS/Pages/Leaderboard/team.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TriviaBotT5.Pages.Leaderboard
+{
+    public class teamModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
